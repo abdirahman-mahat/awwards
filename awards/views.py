f666f23bafb7407ddf4a567028e9543fdbfcafca
abdirahman mahat <abdirahmanmahat3@gmail.com>
2018-11-21 09:57:34 +0300

    search functionality implemented

--- awards/views.py
+++ awards/views.py
@@ -14,19 +14,7 @@ from .permissions import IsAdminOrReadOnly
 # Create your views here.
 
 
-def search_results(request):
-    # check if the input field exists and that ic contains data
-    if 'post' in request.GET and request.GET['post']:
-        # get the data from the search input field
-        explore_posts = Post.all_posts()
-        search_term = request.GET.get('post')
-        searched_posts = Post.filter_by_search_term(search_term)
-        print(search_term)
-        caption = f'Search results for {search_term}'
-
-        if len(searched_posts) == 0:
-            caption = f'Results for {search_term} Found'
-        search_context = {
+dntext = {
             'posts': searched_posts,
             'explore_posts': explore_posts,
             'caption': caption,
